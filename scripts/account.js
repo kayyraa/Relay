1e3ac58baa323fd45fae723a0c346b61574f8eb0
kayyraa <zindhukayrabasbay@gmail.com>
2025-01-11 22:32:42 +0300

    Auto-update from local project folder

---
+++ scripts/account.js
@@ -0,0 +1,46 @@
+import * as Api from "./api.js";
+
+const Form = document.querySelector(".AccountForm");
+if (Form) {
+	const UsernameInput = Form.querySelector(".UsernameInput");
+	const PasswordInput = Form.querySelector(".PasswordInput");
+	const SubmitButton = Form.querySelector(".SubmitButton");
+
+	const Storage = new Api.Storage("Users");
+
+	SubmitButton.addEventListener("click", async () => {
+		const Username = UsernameInput.value;
+		const Password = PasswordInput.value;
+		if (!Username || !Password) return;
+
+		async function GenerateUser() {
+			const Tag = String(Math.floor(Math.random() * 9999) + 1).padStart(4, "0");
+			const ExistingUsers = await Storage.GetDocumentsByField("Username", Username);
+
+			if (ExistingUsers.length > 0) {
+				if (ExistingUsers[0].Password === Password) {
+					localStorage.setItem("User", JSON.stringify(ExistingUsers[0]));
+					window.location.href = "../index.html";
+				}
+			} else {
+				const TagUsers = await Storage.GetDocumentsByField("Tag", Tag);
+				if (TagUsers.length > 0) {
+					await GenerateUser();
+				} else {
+					const User = {
+						Username: Username,
+						Password: Password,
+						Tag: Tag,
+						ProfileImage: ""
+					};
+
+					await Storage.AppendDocument(User);
+					localStorage.setItem("User", JSON.stringify(User));
+					window.location.href = "../index.html";
+				}
+			}
+		}
+
+		await GenerateUser();
+	});
+}
