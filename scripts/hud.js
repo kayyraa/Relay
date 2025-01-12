69e595922bcc059fac351d32117ec4e69661902e
kayyraa <zindhukayrabasbay@gmail.com>
2025-01-12 20:55:19 +0300

    Auto-update from local project folder

---
+++ scripts/hud.js
@@ -0,0 +1,12 @@
+import * as Api from "./api.js";
+
+if (localStorage.getItem("User")) {
+    new Api.Storage("Users").GetDocumentsByField("Username", JSON.parse(localStorage.getItem("User")).Username).then((Users) => {
+        if (Users.length > 0) {
+            Api.LogInButton.innerHTML = "Account";
+            Api.UsernameLabel.innerHTML = Users[0].Username;
+            Api.TagLabel.innerHTML = `#${Users[0].Tag}`;
+            Api.ProfileImage.src = Users[0].ProfileImage ? Users[0].ProfileImage : "../images/Default.svg";
+        } else Api.LogInButton.innerHTML = "Log In";
+    });
+}
